e77bfc5cf0f8f50a38de57dce0de61c9522dceb3
joacomesa22 <joacomesa@gmail.com>
2023-07-18 23:35:05 -0300

    little fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,14 @@ function App() {
   const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
 
   const signUserOut = () => {
-    signOut(auth).then(() => {
-      localStorage.clear();
-      setIsAuth(false);
-      window.location.pathname = "/login";
-    });
+    signOut(auth)
+      .then(() => {
+        localStorage.clear();
+        setIsAuth(false);
+      })
+      .then(() => {
+        window.location.pathname = "/login";
+      });
   };
 
   return (
